44f5b4dbe718844ee8c994ae34b5461710090831
Sw3342_99 <106459398+Kimdudcjf@users.noreply.github.com>
2025-08-05 13:16:01 +0900

    [Silver V] Title: 수들의 합, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -0,0 +1,17 @@
+S = int(input())
+
+left = 1
+right = S
+answer = 0
+
+while left <= right:
+    mid = (left + right) // 2
+    total = mid * (mid + 1) // 2
+
+    if total <= S:
+        answer = mid
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(answer)
