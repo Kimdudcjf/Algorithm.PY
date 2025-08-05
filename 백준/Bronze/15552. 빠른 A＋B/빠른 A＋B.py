8a23413d752fbe504e159470383a252c119f2aa3
Sw3342_99 <106459398+Kimdudcjf@users.noreply.github.com>
2025-08-05 13:47:25 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1340 ms, Memory: 71380 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -0,0 +1,11 @@
+import sys
+
+
+T = int(sys.stdin.readline().rstrip())
+result = []
+for _ in range(T):
+  a, b = map(int, sys.stdin.readline().rstrip().split())
+  result.append(a+b)
+
+for i in range(T):
+  print(result[i])
